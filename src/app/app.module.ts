813b6be6c3140395e173fe942794c52dab7536aa
Sandu Bogdan <sandub69@gmail.com>
2020-12-16 08:04:01 +0200

    login added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -8,6 +8,7 @@ import { ContinutComponent } from './continut/continut.component';
 import { CardComponent } from './card/card.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsComponent } from './forms/forms.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -15,11 +16,15 @@ import { FormsComponent } from './forms/forms.component';
     HeaderComponent,
     ContinutComponent,
     CardComponent,
-    FormsComponent
+    FormsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'login', component: LoginComponent},
+    ]),
     ReactiveFormsModule
   ],
   providers: [],
